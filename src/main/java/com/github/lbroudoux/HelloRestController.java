4f0936ce250237be462e2c74860d3eafd427d116
lbroudoux <laurent.broudoux@gmail.com>
2016-11-22 14:07:53 +0100

    second commit

---
+++ src/main/java/com/github/lbroudoux/HelloRestController.java
@@ -0,0 +1,24 @@
+package com.github.lbroudoux;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lbroudoux
+ */
+@RestController
+@RequestMapping("/api")
+public class HelloRestController {
+
+  private static final String template = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong();
+
+  @RequestMapping(method = RequestMethod.GET, value = "/hello")
+  public Greeting hello(@RequestParam(value="name", defaultValue="World") String name) {
+    return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+  }
+}
